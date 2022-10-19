f6fecba91645f6c735c2f60f0cb3b5b126531c63
El-Banderas <diogobpires@gmail.com>
2022-10-19 17:18:39 +0100

    errors resolved

--- src/utils.c
+++ src/utils.c
@@ -49,25 +49,23 @@ void testIniatialize(){
 // 1(c)
 
 void attribution(int init){
-    
+    // No início de cada atribuição, temos de fingir que os clusters estão vazios.
+    for (int i = 0; i < K; i++){
+        clusterLastPos[i] = 0;
+    } 
     for(int i=init; i<N; i++){
         double clusterMin = (double)RAND_MAX;
         int bestCluster = -1;
-        for (int cluster=0; i<K; i++) {
-            printf("gcX : %f\n", geometricCenterX[i]);
-            printf("gcY : %f\n", geometricCenterY[i]);
-            double distCluster = sqrt(pow(geometricCenterX[i] - x[i], 2) + pow(geometricCenterY[i] - y[i], 2) * 1.0); 
-            printf("distCluster: %f\n",distCluster);
-            printf("clusterMIn: %f\n",clusterMin);
-            printf("bestCluster: %d\n\n", bestCluster);
+        for (int cluster=0; cluster<K; cluster++) {
+            double distCluster = sqrt(pow(geometricCenterX[cluster] - x[cluster], 2) + pow(geometricCenterY[cluster] - y[cluster], 2) * 1.0); 
             if (distCluster < clusterMin) {
                 clusterMin = distCluster;
                 bestCluster = cluster;
             }
         }
         // attribution
-        clusterX[bestCluster][clusterLastPos[bestCluster]+1] = x[i];
-        clusterY[bestCluster][clusterLastPos[bestCluster]+1] = y[i];
+        clusterX[bestCluster][clusterLastPos[bestCluster]] = x[i];
+        clusterY[bestCluster][clusterLastPos[bestCluster]] = y[i];
         clusterLastPos[bestCluster]++;
     }
 }
@@ -102,7 +100,7 @@ void kmeans(){
     // 3,4
     while(numIterations<10) {
 
-        printf("Number of iterations: %d\n", numIterations);
+        printf("Number of iterations : %d\n", numIterations);
 
         attribution(0);
 
