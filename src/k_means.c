4ec684bcbc17eb54d210c1dbe6e39eb3cd61ce04
MiguelAMM42 <a93280@alunos.uminho.pt>
2022-10-24 18:10:04 +0100

    Final correct version . This time right

--- src/k_means.c
+++ src/k_means.c
@@ -6,7 +6,7 @@ int main() {
     initialize();
     //testIniatialize();  
     // começar em K pq o K-1 já estão no cluster
-    //attribution(K);
+    attribution(K);
     kmeans();
     return 0;
 }
